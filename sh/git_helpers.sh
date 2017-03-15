2340fc4d41b08efcde432ab2ceb6604bb6050feb
Mikhail Kovalev <mikhail.kovalev@orbitsoft.com>
2017-03-15 16:18:29 +0300

    add prepare-commit-msg hook

--- sh/git_helpers.sh
+++ sh/git_helpers.sh
@@ -34,3 +34,35 @@ function deleteBranchesByPattenr() {
 		echo 'Enter pattern!'
 	fi
 }
+
+function setHooks() {
+	for d in */ ; do
+		echo -e "\e[42m $d \e[0m"
+
+		cd "$d"
+
+		read -p "Replace hooks in? " -n 1 -r
+		echo    # (optional) move to a new line
+		if [[ $REPLY =~ ^[Yy]$ ]]
+		then
+		    ln -sf /home/local/opt/zz_customs/git-hooks/hooks/prepare-commit-msg  .git/hooks/
+		fi
+
+		read -p "Replace hooks in subdirs? " -n 1 -r
+		echo    # (optional) move to a new line
+		if [[ $REPLY =~ ^[Yy]$ ]]
+		then
+		    for ds in */ ; do
+		    	echo -e "    \e[42m $ds \e[0m"
+		    	
+		    	cd "$ds"
+		    	
+		    	ln -sf /home/local/opt/zz_customs/git-hooks/hooks/prepare-commit-msg  .git/hooks/
+		    	
+		    	cd .. 
+		    done
+		fi
+
+		cd ..
+	done
+}
